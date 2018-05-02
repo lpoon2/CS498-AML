8b740d45e4d11658a3ad466a5090c4628701f6ee
Titus F <titusfong@gmail.com>
2018-05-01 22:55:54 -0500

    Add files via upload

--- hw9/mnist.improved.py
+++ hw9/mnist.improved.py
@@ -58,7 +58,7 @@ def cnn_model_fn(features, labels, mode):
       # Densely connected layer with 1024 neurons
       # Input Tensor Shape: [batch_size, 7 * 7 * 64]
       # Output Tensor Shape: [batch_size, 1024]
-    dense_1 = tf.layers.dense(inputs=pool2_flat, units=2048, activation=tf.nn.relu)
+    dense_1 = tf.layers.dense(inputs=pool3_flat, units=2048, activation=tf.nn.relu)
 
       # Add dropout operation; 0.6 probability that element will be kept
     dropout_1 = tf.layers.dropout(
