b0f8853210df121b4ac6341bc787fe793cb9a0e5
Titus F <titusfong@gmail.com>
2018-05-08 01:29:20 -0500

    Update cifar10_improved_3.py

--- hw9/cifar10_improved_3.py
+++ hw9/cifar10_improved_3.py
@@ -243,7 +243,7 @@ def inference(images):
   # local4
   with tf.variable_scope('local4') as scope:
     # Move everything into depth so we can perform a single matrix multiply.
-    reshape = tf.reshape(norm2, [images.get_shape().as_list()[0], -1])
+    reshape = tf.reshape(pool2, [images.get_shape().as_list()[0], -1])
     dim = reshape.get_shape()[1].value
     weights = _variable_with_weight_decay('weights', shape=[dim, 2048],
                                           stddev=0.04, wd=0.004)
